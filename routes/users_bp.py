3712c27e4f7ee202ab48520b5eaa6c05dc1740a6
veeramani2004 <veeramanipalanisamy642004@gmail.com>
2025-09-02 23:42:12 +0530

    hassing added for login and  singnup

--- routes/users_bp.py
+++ routes/users_bp.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from models.user import User
 from extensions import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 users_bp = Blueprint("users_bp", __name__)
 
@@ -13,7 +13,18 @@ HTTP_ERROR = 500
 @users_bp.post("/signup")
 def create_user():
     data = request.get_json()
-    new_user = User(**data)
+    username = data.get("username")  # None (falsy) # postman
+    password = data.get("password")  # "" (falsy) # postman
+
+    if not username or not password:
+        return {"error": "username / password required"}, 400
+
+    db_user = User.query.filter_by(username=username).first()  # None
+
+    if db_user:
+        return {"error": "username already taken"}, 400
+
+    new_user = User(username=username, password=generate_password_hash(password))
     try:
         db.session.add(new_user)
         db.session.commit()
@@ -39,7 +50,7 @@ def login_user():
     db_user = db_user.to_dict()
 
     # Password Check
-    if db_user.get("password") != password:
+    if not check_password_hash(db_user.get("password"), password):
         return {"error": "username or password is incorrect"}, 401
 
     print("login ✨✨✨✨")
